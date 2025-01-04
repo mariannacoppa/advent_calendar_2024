6fadc793e312a5830e46e6f3d9b39c465e07f8d6
mariannacoppa <densimetra@gmail.com>
2025-01-04 19:22:31 +0100

    [UPD] keep cells clicked even after pagerefreshing using localStorage

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,31 +1,50 @@
 <script>
 import daysItems from "../../daysItems.json";
 import DayItem from "./DayItem.vue";
+
 export default {
   components: {
     DayItem,
   },
   data() {
     return {
-      days: daysItems,
+      days: daysItems.map((day, index) => ({
+        ...day,
+        // add "covered" cell state
+        covered:
+          JSON.parse(localStorage.getItem(`day-${index}-covered`)) || false,
+      })),
     };
+  },
+  methods: {
+    // when clicked, each cell change will be memorized in localStorage in order to be visible even after page refreshing
+    toggleCover(index) {
+      this.days[index].covered = !this.days[index].covered;
+      localStorage.setItem(`day-${index}-covered`, this.days[index].covered);
+    },
   },
 };
 </script>
+
 <template>
   <div class="bg-darkblue">
     <main class="container">
       <div class="row">
+        <!-- .native changer on dayItem click event spreads the event to the father component -->
         <DayItem
           class="col-3 col-md-2 d-flex justify-content-center"
           v-for="(dayItem, index) in days"
+          :key="index"
           :icon="dayItem.icon"
           :number="dayItem.number"
+          :covered="dayItem.covered"
+          @click.native="toggleCover(index)"
         />
       </div>
     </main>
   </div>
 </template>
+
 <style lang="scss" scoped>
 .bg-darkblue {
   background-color: #100b45;
--- src/components/DayItem.vue
+++ src/components/DayItem.vue
@@ -3,32 +3,56 @@ export default {
   props: {
     icon: String,
     number: String,
+    covered: Boolean,
   },
 };
 </script>
+
 <template>
-  <div>
-    <div class="day-item align-content-center mt-2 rounded-3">
+  <div class="day-item-container">
+    <div
+      class="day-item align-content-center mt-2 rounded-3"
+      :class="{ covered }"
+    >
       <div class="d-flex justify-content-center">
         <img :src="icon" alt="" class="h-25 mt-2" />
       </div>
       <div class="number text-center fs-1">{{ number }}</div>
     </div>
+    <div v-if="covered" class="overlay"></div>
   </div>
 </template>
+
 <style lang="scss">
+.day-item-container {
+  position: relative;
+}
+
 .day-item {
   background-color: #296150;
   width: 75%;
 }
+
 .day-item:hover {
   cursor: pointer;
 }
+
 .day-item:active {
   background-color: #100b45;
 }
+
 img {
   width: 40%;
   height: 40%;
+}
+/*add clicked cell overlay*/
+.overlay {
+  position: absolute; /*covers the cell*/
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 8px;
 }
 </style>
